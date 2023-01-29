c392a40ba9e20fa0bf13d122f539ca84e342b6fa
Viktor Holta <viktor.holta@outlook.com>
2023-01-29 17:55:19 +0100

    NEW: fetch user & chat data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,56 @@
-<div class="app">
-    <main>
-        <slot />
-    </main>
-</div>
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import type { Unsubscriber } from 'svelte/store';
+    import { chats, user } from '../api/data';
+	import { fetchFriendChats } from '../api/friend';
+	import { fetchGroupChats } from '../api/group';
+	import { fetchMyself } from '../api/user';
+
+    let userUnsub: Unsubscriber | undefined;
+
+    onMount(() => {
+        fetchMyself();
+
+        userUnsub = user.subscribe(async value => {
+            await fetchFriendChats();
+            fetchGroupChats();
+        });
+    });
+
+    onDestroy(() => {
+        if (userUnsub !== undefined)
+            userUnsub();
+    });
+</script>
+
+<nav>
+    {#each $chats as chat}
+        <p>{chat.name}</p>
+    {/each}
+</nav>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    nav {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        width: 16rem;
+
+        background-color: #263238;
+    }
+
+    main {
+        position: absolute;
+        top: 0;
+        left: 16rem;
+        bottom: 0;
+        right: 0;
+
+        background-color: #37474F;
+    }
+</style>
